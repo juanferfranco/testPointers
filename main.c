e72a4443a46ffb2bf4d8aa47c780bd00aa743854
JuanF <juanf@hola.com>
2022-02-23 09:05:54 -0500

    add print with pointer

--- main.c
+++ main.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+void printArr( uint8_t *parr, uint8_t size);
+
+
 int main(void)
 {
+    uint8_t arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15};
 
-    uint8_t arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (uint8_t counter = 0; counter < 10; counter = counter + 1)
-    {
-        printf("arr[%d]: %d\n", counter, arr[counter]);
-    }
+    printArr(  &arr[0] , sizeof(arr)/sizeof(uint8_t));
 
     return EXIT_SUCCESS;
+}
+/*
+void printArr( uint8_t *parr, uint8_t size){
+    for (uint8_t counter = 0; counter < size; counter = counter + 1)
+    {
+        printf("arr[%d]: %d\n", counter, parr[counter]);
+    }
+}
+*/
+
+void printArr( uint8_t *parr, uint8_t size){
+    for (uint8_t counter = 0; counter < size; counter = counter + 1)
+    {
+        printf("arr[%d]: %d\n", counter, *(parr + counter)  );
+    }
 }
