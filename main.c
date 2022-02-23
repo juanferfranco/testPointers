327dbd73abf939b44608cc6a0daef3f536f2564a
JuanF <juanf@hola.com>
2022-02-23 08:48:14 -0500

    change while to for

--- main.c
+++ main.c
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int main(void){
+int main(void)
+{
 
-    uint8_t arr[] = {1,2,3,4,5,6,7,8,9,10};
-
-    uint8_t counter = 0;
-
-    while( counter < 10){
+    uint8_t arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (uint8_t counter = 0; counter < 10; counter = counter + 1)
+    {
         printf("arr[%d]: %d\n", counter, arr[counter]);
-        counter = counter + 1;
     }
 
     return EXIT_SUCCESS;
 }
-
