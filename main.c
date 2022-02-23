38a51202e818ad036fd41dd166aac67f3e64fe5e
JuanF <juanf@hola.com>
2022-02-23 08:41:59 -0500

    add while to print the array

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(void){
+
+    uint8_t arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+    uint8_t counter = 0;
+
+    while( counter < 10){
+        printf("arr[%d]: %d\n", counter, arr[counter]);
+        counter = counter + 1;
+    }
 
     return EXIT_SUCCESS;
 }
